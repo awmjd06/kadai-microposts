d1d90d8cdce4b8cf6ac80640515f8bce2a203f0f
mai.oono <lilac.jpg@gmail.com>
2019-08-10 21:14:41 +0000

    favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, length: { maximum: 255 }
+  has_many :reverses_of_favorite, class_name: "favorite"
+  has_many :liked, through: :reverses_of_favorite, source: :user
+
 end
